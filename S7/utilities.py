f3030b9bddc667a1fda06de1f2096d216b3c70ff
Akhil Poojary <akhilp.2597@gmail.com>
2021-01-24 05:19:03 +0530

    Update utilities.py

--- S7/utilities.py
+++ S7/utilities.py
@@ -77,3 +77,86 @@ def plot_images(loader, rows=5, cols=5,mean=(0,0,0),std=(1,1,1),classes=[0,0,0])
         ax.imshow(images[i].numpy().transpose((1,2,0))*std + mean)
         ax.set_title('Label: {}'.format(classes[labels[i]]))
     plt.show()
+
+def plot_loss(train_loss_vals,test_loss_vals,epochs)
+    x = [i for i in range(0,epochs)]
+    y_train = train_loss_vals
+    y_test = test_loss_vals
+
+    loss = plt.figure()
+    plt.title("Train/Validation Loss on CIFAR 10")
+    plt.xlabel("Epochs")
+    plt.ylabel("Validation Loss")
+
+    plt.plot(x, y_train, color='b')
+    plt.plot(x, y_test, color='r')
+
+    plt.show()
+    my_dpi = 100
+    loss.savefig('S7/loss.png',figsize=(800/my_dpi, 800/my_dpi), dpi=my_dpi)
+
+
+def plot_acc(train_acc_vals,test_acc_vals,epochs)
+    x = [i for i in range(0,epochs)]
+    y_train = train_acc_vals
+    y_test = test_acc_vals
+
+    acc = plt.figure()
+    plt.title("Train/Validation Accuracy on CIFAR 10")
+    plt.xlabel("Epochs")
+    plt.ylabel("Validation Accuracy")
+
+    plt.plot(x, y_train, color='b')
+    plt.plot(x, y_test, color='r')
+
+    plt.show()
+    my_dpi = 100
+    acc.savefig('S7/accuracy.png',figsize=(800/my_dpi, 800/my_dpi), dpi=my_dpi)
+
+def plot_misclassified(Net,MODEL_PATH, rows=5, cols=5, mean=(0,0,0), std=(1,1,1), classes=[0,0,0]):
+    num_row     = 5
+    num_col     = 5
+    num_images  = num_row*num_col
+    batch_size_test = config.BATCH_SIZE_TEST
+
+    # Empty lists for storing the misclassified images, their corresponding predicitons and labels
+    mis_images = []
+    mis_labels = []
+    mis_pred   = []
+
+    # MODEL_PATH = "S7/models/S7_best_model.model"
+    model = Net(BN_type='BN')
+    model.load_state_dict(torch.load(MODEL_PATH))
+    model = model.to(device)
+    model.eval()
+
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            images,labels  = data.to(device), target.to(device)
+            output         = model(images)
+            pred           = (output.argmax(dim=1, keepdim=True)).view_as(labels)
+            mis_classified = pred.ne(labels).tolist()
+            # 'mis_classified' is a Boolean list (size same as labels). 
+            # It is True where the prediction does not match label
+            if any(mis_classified) == True:
+                mis_images.extend(images[mis_classified].cpu())
+                mis_labels.extend(labels[mis_classified].cpu())
+                mis_pred.extend(pred[mis_classified].cpu())
+            
+            if len(mis_pred)>=num_images:
+                mis_images = mis_images[:25]
+                mis_labels = mis_labels[:25]
+                mis_pred = mis_pred[:25]
+                break
+
+    # Plot the digit images with label and predictions
+    fig, axes = plt.subplots(num_row, num_col, figsize=(1.8*num_col,2.5*num_row))
+    for i in range(num_images):
+        ax = axes[i//num_col, i%num_col]
+        ax.imshow(mis_images[i].numpy().transpose((1,2,0))*std + mean)
+        ax.set_title('Label: {}\nPrediction: {}'.format(classes[mis_labels[i]],
+                                                        classes[mis_pred[i]]))
+
+    plt.show()
+    my_dpi = 100
+    fig.savefig('S7/S7_misclassified_images.png',figsize=(800/my_dpi, 800/my_dpi), dpi=my_dpi)

8150548c8f0593c1904003abcd46ae7d62e52cd5
Akhil Poojary <akhilp.2597@gmail.com>
2021-01-23 17:02:08 +0530

    Configuration Parameters

---
+++ S7/config.py
@@ -0,0 +1,13 @@
+import torch
+
+USE_CUDE = torch.cuda.is_available()
+DEVICE   = torch.device("cuda" if use_cuda else "cpu")
+
+BATCH_SIZE_TRAIN  = 64
+BATCH_SIZE_TEST   = 512
+num_workers = 0
+pin_memory  = True
+input_size_MNIST   = (1, 28, 28)
+input_size_CIFAR10 = (3, 32, 32)
+
+CIFAR_classes = ('plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse', 'ship', 'truck')

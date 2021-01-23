cba085a702ccd3a5a8ebd35d4a282d7b96a5c78a
Akhil Poojary <akhilp.2597@gmail.com>
2021-01-24 01:10:09 +0530

    Update config.py

--- S7/config.py
+++ S7/config.py
@@ -13,7 +13,7 @@ CIFAR_classes = ('plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse', '
 
 to_tensor = transforms.Compose([transforms.ToTensor()])
 
-train_transform  = transforms.Compose([torchvision.transforms.RandomAffine(degrees=8, translate=(0.1,0.1), scale=(0.95,1.05))
+train_transform  = transforms.Compose([transforms.RandomAffine(degrees=8, translate=(0.1,0.1), scale=(0.95,1.05))
                                           ,transforms.ToTensor()
                                           ,transforms.Normalize((0.49186122, 0.48266134, 0.44720834), (0.24699295, 0.24340236, 0.26160896))])
 

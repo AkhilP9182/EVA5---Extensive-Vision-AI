914c58e517bad0fdc436fb139d78804f056a9c27
Akhil Poojary <akhilp.2597@gmail.com>
2021-01-24 00:23:56 +0530

    Update utilities.py

--- S7/utilities.py
+++ S7/utilities.py
@@ -4,7 +4,7 @@ import torchvision
 import torchvision.datasets
 import torch.utils.data
 import torchvision.transforms as transforms
-import matplotlin.pyplot as plt
+import matplotlib.pyplot as plt
 import config
 
 to_tensor = transforms.Compose([transforms.ToTensor()])

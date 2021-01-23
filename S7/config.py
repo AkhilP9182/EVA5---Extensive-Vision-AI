27fc864b4f3ac28b4e6e4ae86665e62494b2d973
Akhil Poojary <akhilp.2597@gmail.com>
2021-01-24 01:04:52 +0530

    Config with Transforms

--- S7/config.py
+++ S7/config.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision.transforms as transforms
 
 USE_CUDA = torch.cuda.is_available()
 DEVICE   = torch.device("cuda" if USE_CUDA else "cpu")
@@ -6,7 +7,15 @@ DEVICE   = torch.device("cuda" if USE_CUDA else "cpu")
 BATCH_SIZE_TRAIN  = 64
 BATCH_SIZE_TEST   = 512
 num_workers = 0
-pin_memory = True
 input_size_CIFAR10 = (3, 32, 32)
 
 CIFAR_classes = ('plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+to_tensor = transforms.Compose([transforms.ToTensor()])
+
+train_transform  = transforms.Compose([torchvision.transforms.RandomAffine(degrees=8, translate=(0.1,0.1), scale=(0.95,1.05))
+                                          ,transforms.ToTensor()
+                                          ,transforms.Normalize((0.49186122, 0.48266134, 0.44720834), (0.24699295, 0.24340236, 0.26160896))])
+
+test_transform = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize((0.49186122, 0.48266134, 0.44720834), (0.24699295, 0.24340236, 0.26160896))])

1f8f5ff4080aef1e96c5d88d8ad14a492af998d5
Akhil Poojary <akhilp.2597@gmail.com>
2021-01-30 13:36:04 +0530

    Update config.py

--- S8/config.py
+++ S8/config.py
@@ -9,7 +9,7 @@ DEVICE   = torch.device("cuda" if USE_CUDA else "cpu")
 BATCH_SIZE_TRAIN  = 64
 BATCH_SIZE_TEST   = 1024
 EPOCHS   = 30
-LR       = 0.005
+LR       = 0.02
 MOMENTUM = 0.9
 num_workers = 4
 LR_STEP = 4
@@ -20,8 +20,8 @@ CIFAR_classes = ('plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse', '
 
 to_tensor = transforms.Compose([transforms.ToTensor()])
 
-train_transform  = transforms.Compose([transforms.RandomHorizontalFlip(p=0.3)
-                                       ,transforms.RandomAffine(degrees=6, translate=(0.05,0.05), scale=(0.95,1.05))
+train_transform  = transforms.Compose([transforms.RandomHorizontalFlip(p=0.5)
+                                       ,transforms.RandomAffine(degrees=8, translate=(0.05,0.05), scale=(0.95,1.05))
                                        ,transforms.ToTensor()
                                        ,transforms.Normalize((0.49186122, 0.48266134, 0.44720834), (0.24699295, 0.24340236, 0.26160896))])
 

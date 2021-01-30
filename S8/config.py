55e4335a84f5a091a85b7c47c364e3638a77cd1f
Akhil Poojary <akhilp.2597@gmail.com>
2021-01-30 11:36:51 +0530

    Update config.py

--- S8/config.py
+++ S8/config.py
@@ -6,13 +6,13 @@ import torchvision.transforms as transforms
 USE_CUDA = torch.cuda.is_available()
 DEVICE   = torch.device("cuda" if USE_CUDA else "cpu")
 
-BATCH_SIZE_TRAIN  = 128
+BATCH_SIZE_TRAIN  = 64
 BATCH_SIZE_TEST   = 1024
-EPOCHS   = 25
-LR       = 0.001
+EPOCHS   = 30
+LR       = 0.01
 MOMENTUM = 0.9
 num_workers = 4
-LR_STEP = 5
+LR_STEP = 4
 LR_GAMMA = 0.8
 input_size_CIFAR10 = (3, 32, 32)
 
@@ -20,9 +20,10 @@ CIFAR_classes = ('plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse', '
 
 to_tensor = transforms.Compose([transforms.ToTensor()])
 
-train_transform  = transforms.Compose([transforms.RandomAffine(degrees=6, translate=(0.05,0.05), scale=(0.95,1.05))
-                                          ,transforms.ToTensor()
-                                          ,transforms.Normalize((0.49186122, 0.48266134, 0.44720834), (0.24699295, 0.24340236, 0.26160896))])
+train_transform  = transforms.Compose([transforms.RandomHorizontalFlip(p=0.3)
+                                       ,transforms.RandomAffine(degrees=6, translate=(0.05,0.05), scale=(0.95,1.05))
+                                       ,transforms.ToTensor()
+                                       ,transforms.Normalize((0.49186122, 0.48266134, 0.44720834), (0.24699295, 0.24340236, 0.26160896))])
 
-test_transform = transforms.Compose([transforms.ToTensor(),
-                                         transforms.Normalize((0.49186122, 0.48266134, 0.44720834), (0.24699295, 0.24340236, 0.26160896))
+test_transform = transforms.Compose([transforms.ToTensor()
+                                     ,transforms.Normalize((0.49186122, 0.48266134, 0.44720834), (0.24699295, 0.24340236, 0.26160896))

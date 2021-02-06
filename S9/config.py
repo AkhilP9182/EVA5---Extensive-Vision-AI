1bf87035b2cbbcf6f0fee82cc7b6abed1a8e85f5
Akhil Poojary <akhilp.2597@gmail.com>
2021-02-07 00:10:31 +0530

    Add files via upload

---
+++ S9/config.py
@@ -0,0 +1,29 @@
+import torch
+import torchvision.transforms as transforms
+
+# Following are the conguration parameters which will be used throughout the code
+
+USE_CUDA = torch.cuda.is_available()
+DEVICE   = torch.device("cuda" if USE_CUDA else "cpu")
+
+BATCH_SIZE_TRAIN  = 64
+BATCH_SIZE_TEST   = 1024
+EPOCHS   = 30
+LR       = 0.002
+MOMENTUM = 0.9
+num_workers = 4
+LR_STEP = 4
+LR_GAMMA = 0.8
+input_size_CIFAR10 = (3, 32, 32)
+
+CIFAR_classes = ('plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+to_tensor = transforms.Compose([transforms.ToTensor()])
+
+train_transform  = transforms.Compose([transforms.RandomHorizontalFlip(p=0.5)
+                                       ,transforms.RandomAffine(degrees=8, translate=(0.05,0.05), scale=(0.95,1.05))
+                                       ,transforms.ToTensor()
+                                       ,transforms.Normalize((0.49186122, 0.48266134, 0.44720834), (0.24699295, 0.24340236, 0.26160896))])
+
+test_transform = transforms.Compose([transforms.ToTensor()
+                                     ,transforms.Normalize((0.49186122, 0.48266134, 0.44720834), (0.24699295, 0.24340236, 0.26160896))])
---
+++ S9/utilities.py
@@ -0,0 +1,214 @@
+import numpy as np
+import torch
+import torchvision
+import torch.utils.data
+import matplotlib.pyplot as plt
+import S8.config as config
+
+def train_loader_cifar10(trainset, shuffle=True, num_workers=2):
+    """
+    Function for getting a trainloader iterator
+    """
+    trainloader      = torch.utils.data.DataLoader(trainset, batch_size = config.BATCH_SIZE_TRAIN,
+                                                    shuffle=shuffle, num_workers=config.num_workers)
+    return trainloader
+
+def test_loader_cifar10(testset, shuffle=False, num_workers=2):
+    """
+    Function for getting a testloader iterator
+    """
+
+    testloader     = torch.utils.data.DataLoader(testset, batch_size = config.BATCH_SIZE_TEST,
+                                                shuffle=shuffle, num_workers=config.num_workers)
+
+    return testloader
+
+def get_mean_std_overall(trainset,testset):
+    """
+    Function for getting the mean and standard devitation of dataset (train and test combined)
+    """
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size = 1024, shuffle=False, num_workers=1)
+    test_loader  = torch.utils.data.DataLoader(testset, batch_size = 1024, shuffle=False, num_workers=1)
+    channel_sum, channel_squared_sum, num_batches = 0,0,0
+
+    for data, _ in train_loader:
+        channel_sum += torch.mean(data, dim=[0,2,3])
+        channel_squared_sum += torch.mean(data**2, dim=[0,2,3])
+        num_batches += 1
+
+    for data, _ in test_loader:
+        channel_sum += torch.mean(data, dim=[0,2,3])
+        channel_squared_sum += torch.mean(data**2, dim=[0,2,3])
+        num_batches += 1
+
+    mean = (channel_sum/num_batches)
+    std  = ((channel_squared_sum/num_batches) - mean**2)**0.5
+    return mean,std
+
+def dataset_info(train_set,test_set):
+    """
+        The following 7 lines are to assert whether both training and test sets have the same number/type of 
+        classes (with the same labelling) for classification, and assign the number to a variable 
+        'num_classes' which will be equal to the number of kernel that will be used later in the 
+        final convolution layer.
+    """
+    classes_in_train = list(set(train_set.targets))
+    classes_in_test  = list(set(test_set.targets))
+    assert np.isin(classes_in_test,classes_in_train).all()
+    num_classes = len(set(train_set.targets))
+    print("Number of classes in CIFAR10   : {}".format(num_classes))
+    print("Number of images for training  : {}".format(len(train_set)))
+    print("Number of images for validation: {}".format(len(test_set)))
+    return "Data loading done!"
+
+def plot_images(loader, rows=5, cols=5,mean=(0,0,0),std=(1,1,1),classes=[0,0,0]):
+    dataiter = iter(loader)
+    images, labels = dataiter.next()
+
+    num_row     = rows
+    num_col     = cols
+    num_images  = num_row*num_col
+
+    fig, axes = plt.subplots(num_row, num_col, figsize=(1.8*num_col,2.5*num_row))
+    for i in range(num_images):
+        ax = axes[i//num_col, i%num_col]
+        ax.imshow(images[i].numpy().transpose((1,2,0))*std + mean)
+        ax.set_title('Label: {}'.format(classes[labels[i]]))
+    plt.show()
+
+def plot_loss(train_loss_vals,test_loss_vals,epochs):
+    x = [i for i in range(0,epochs)]
+    y_train = train_loss_vals
+    y_test = test_loss_vals
+
+    loss = plt.figure()
+    plt.title("Train/Validation Loss on CIFAR 10")
+    plt.xlabel("Epochs")
+    plt.ylabel("Validation Loss")
+
+    plt.plot(x, y_train, color='b',label="Training")
+    plt.plot(x, y_test, color='r',label="Validation")
+    plt.legend(loc ="upper right")
+
+    plt.show()
+    my_dpi = 100
+    loss.savefig('S8/S8_loss.png',figsize=(800/my_dpi, 800/my_dpi), dpi=my_dpi)
+
+
+def plot_acc(train_acc_vals,test_acc_vals,epochs):
+    x = [i for i in range(0,epochs)]
+    y_train = train_acc_vals
+    y_test = test_acc_vals
+
+    acc = plt.figure()
+    plt.title("Train/Validation Accuracy on CIFAR 10")
+    plt.xlabel("Epochs")
+    plt.ylabel("Validation Accuracy")
+
+    plt.plot(x, y_train, color='b',label="Training")
+    plt.plot(x, y_test, color='r',label="Validation")
+    plt.legend(loc ="lower right")
+
+    plt.show()
+    my_dpi = 100
+    acc.savefig('S8/S8_accuracy.png',figsize=(800/my_dpi, 800/my_dpi), dpi=my_dpi)
+
+def plot_misclassified(Net,MODEL_PATH,test_loader,
+                        rows=5, cols=5, mean=(0,0,0), std=(1,1,1), classes=[0,0,0]):
+    num_row     = 5
+    num_col     = 5
+    num_images  = num_row*num_col
+    batch_size_test = config.BATCH_SIZE_TEST
+
+    # Empty lists for storing the misclassified images, their corresponding predicitons and labels
+    mis_images = []
+    mis_labels = []
+    mis_pred   = []
+    device = config.DEVICE
+
+    # MODEL_PATH = "S8/models/S8_best_resnet18_model.model"
+    model = Net()
+    model.load_state_dict(torch.load(MODEL_PATH))
+    model = model.to(device)
+    model.eval()
+
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            images,labels  = data.to(device), target.to(device)
+            output         = model(images)
+            pred           = (output.argmax(dim=1, keepdim=True)).view_as(labels)
+            mis_classified = pred.ne(labels).tolist()
+            # 'mis_classified' is a Boolean list (size same as labels). 
+            # It is True where the prediction does not match label
+            if any(mis_classified) == True:
+                mis_images.extend(images[mis_classified].cpu())
+                mis_labels.extend(labels[mis_classified].cpu())
+                mis_pred.extend(pred[mis_classified].cpu())
+            
+            if len(mis_pred)>=num_images:
+                mis_images = mis_images[:25]
+                mis_labels = mis_labels[:25]
+                mis_pred = mis_pred[:25]
+                break
+
+    # Plot the digit images with label and predictions
+    fig, axes = plt.subplots(num_row, num_col, figsize=(1.8*num_col,2.5*num_row))
+    for i in range(num_images):
+        ax = axes[i//num_col, i%num_col]
+        ax.imshow(mis_images[i].numpy().transpose((1,2,0))*std + mean)
+        ax.set_title('Label: {}\nPrediction: {}'.format(classes[mis_labels[i]],
+                                                        classes[mis_pred[i]]))
+
+    plt.show()
+    my_dpi = 100
+    fig.savefig('S8/S8_misclassified_images.png',figsize=(800/my_dpi, 800/my_dpi), dpi=my_dpi)
+    
+def plot_correct_classified(Net,MODEL_PATH,test_loader,
+                        rows=5, cols=5, mean=(0,0,0), std=(1,1,1), classes=[0,0,0]):
+    num_row     = 5
+    num_col     = 5
+    num_images  = num_row*num_col
+    batch_size_test = config.BATCH_SIZE_TEST
+
+    # Empty lists for storing the misclassified images, their corresponding predicitons and labels
+    correct_images = []
+    correct_labels = []
+    correct_pred   = []
+    device = config.DEVICE
+
+    # MODEL_PATH = "S8/models/S8_best_resnet18_model.model"
+    model = Net()
+    model.load_state_dict(torch.load(MODEL_PATH))
+    model = model.to(device)
+    model.eval()
+
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            images,labels  = data.to(device), target.to(device)
+            output         = model(images)
+            pred           = (output.argmax(dim=1, keepdim=True)).view_as(labels)
+            correct_classified = pred.eq(labels).tolist()
+            # 'mis_classified' is a Boolean list (size same as labels). 
+            # It is True where the prediction does not match label
+            if any(correct_classified) == True:
+                correct_images.extend(images[correct_classified].cpu())
+                correct_labels.extend(labels[correct_classified].cpu())
+                correct_pred.extend(pred[correct_classified].cpu())
+            
+            if len(correct_pred)>=num_images:
+                correct_images = correct_images[:25]
+                correct_labels = correct_labels[:25]
+                correct_pred = correct_pred[:25]
+                break
+
+    # Plot the digit images with label and predictions
+    fig, axes = plt.subplots(num_row, num_col, figsize=(1.8*num_col,2.5*num_row))
+    for i in range(num_images):
+        ax = axes[i//num_col, i%num_col]
+        ax.imshow(correct_images[i].numpy().transpose((1,2,0))*std + mean)
+        ax.set_title('Label: {}\nPrediction: {}'.format(classes[correct_labels[i]],
+                                                        classes[correct_pred[i]]))
+
+    plt.show()
+    my_dpi = 100
+    fig.savefig('S8/S8_correct_classified_images.png',figsize=(800/my_dpi, 800/my_dpi), dpi=my_dpi)
